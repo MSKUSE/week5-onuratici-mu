4dd0a6dd79602e47348ed8ea8589ee805e527ab0
onuratici-mu <onuratici@posta.mu.edu.tr>
2025-03-21 12:10:13 +0300

    Add files via upload

--- week5/src/Main.java
+++ week5/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Point p1 = new Point();
+        String s1 = new String("asd");
+        System.out.println(p1.xCoor);
+        System.out.println(p1.yCoor);
+
+        Point p2 = new Point(3,4);
+        System.out.println(p2.xCoor);
+        System.out.println(p2.yCoor);
+
+        Point p3 = new Point(5);
+        System.out.println(p3.xCoor);
+        System.out.println(p3.yCoor);
+
+        Rectangle r1 = new Rectangle(p3, 4 ,5);
+        System.out.println(r1.getSideA());
+        System.out.println(r1.getSideB());
+        System.out.println(r1.getTopLeft().xCoor);
+        System.out.println(r1.getTopLeft().yCoor);
+
+        System.out.println(r1.perimeter());
+        System.out.println(r1.area());
     }
+
 }
---
+++ week5/src/Point.java
@@ -0,0 +1,17 @@
+public class Point {
+
+    public int xCoor, yCoor;
+
+    public Point(){
+
+    }
+    public Point(int x, int y){
+        this.xCoor = x;
+        this.yCoor = y;
+    }
+
+    public Point(int x) {
+        this.xCoor = x;
+        this.yCoor = x;
+    }
+}
